8eca0baece7e434f6add0e4cc8a7b420f80af487
Binaek Sarkar <binaek@turbot.com>
2022-06-29 20:18:50 +0530

    update user roles table

--- crowdstrike/table_crowdstrike_user_role.go
+++ crowdstrike/table_crowdstrike_user_role.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/crowdstrike/gofalcon/falcon"
+	"github.com/crowdstrike/gofalcon/falcon/client"
 	"github.com/crowdstrike/gofalcon/falcon/client/user_management"
+	"github.com/crowdstrike/gofalcon/falcon/models"
 	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
@@ -22,12 +24,12 @@ func tableCrowdStrikeUserRole(_ context.Context) *plugin.Table {
 			KeyColumns: plugin.AllColumns([]string{"id"}),
 		},
 		Columns: []*plugin.Column{
-			{Name: "id", Description: "TODO", Type: proto.ColumnType_STRING, Hydrate: getCrowdStrikeUserRole},
-			{Name: "cid", Description: "TODO.", Type: proto.ColumnType_STRING, Hydrate: getCrowdStrikeUserRole},
-			{Name: "description", Description: "TODO.", Type: proto.ColumnType_STRING, Hydrate: getCrowdStrikeUserRole},
-			{Name: "display_name", Description: "TODO.", Type: proto.ColumnType_STRING, Hydrate: getCrowdStrikeUserRole},
+			{Name: "id", Description: "TODO", Type: proto.ColumnType_STRING},
+			{Name: "cid", Description: "TODO.", Type: proto.ColumnType_STRING},
+			{Name: "description", Description: "TODO.", Type: proto.ColumnType_STRING},
+			{Name: "display_name", Description: "TODO.", Type: proto.ColumnType_STRING},
 			// Steampipe standard columns
-			{Name: "title", Description: "Title of the resource.", Type: proto.ColumnType_STRING, Transform: transform.FromField("Cid")},
+			{Name: "title", Description: "Title of the resource.", Type: proto.ColumnType_STRING, Transform: transform.FromField("DisplayName")},
 		},
 	}
 }
@@ -48,7 +50,7 @@ func listCrowdStrikeUserRole(ctx context.Context, d *plugin.QueryData, h *plugin
 	}
 
 	response, err := client.UserManagement.GetAvailableRoleIds(
-		user_management.NewGetAvailableRoleIdsParams().WithContext(ctx),
+		user_management.NewGetAvailableRoleIdsParamsWithContext(ctx),
 	)
 
 	if err != nil {
@@ -59,18 +61,18 @@ func listCrowdStrikeUserRole(ctx context.Context, d *plugin.QueryData, h *plugin
 	}
 
 	roleIdbatch := response.Payload.Resources
-
-	for _, roleId := range roleIdbatch {
-		d.StreamListItem(ctx, roleIdStruct{
-			RoleId: roleId,
-		})
-		if d.QueryStatus.RowsRemaining(ctx) < 1 {
-			return nil, nil
+	if roles, err := getUserRolesByIds(ctx, client, roleIdbatch); err == nil {
+		for _, role := range roles {
+			d.StreamListItem(ctx, role)
+			if d.QueryStatus.RowsRemaining(ctx) < 1 {
+				return nil, nil
+			}
 		}
 	}
 
 	return nil, nil
 }
+
 func getCrowdStrikeUserRole(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	client, err := getCrowdStrikeClient(ctx, d)
 	if err != nil {
@@ -79,26 +81,30 @@ func getCrowdStrikeUserRole(ctx context.Context, d *plugin.QueryData, h *plugin.
 	}
 
 	var roleId string
+	roleId = d.KeyColumnQuals["id"].GetStringValue()
 
-	if h.Item != nil {
-		result := h.Item.(roleIdStruct)
-		roleId = result.RoleId
-	} else {
-		roleId = d.KeyColumnQuals["id"].GetStringValue()
-	}
-
-	if err := getRateLimiter(ctx, d).Wait(ctx); err != nil {
-		return nil, err
-	}
-
-	response, err := client.UserManagement.GetRoles(
-		user_management.NewGetRolesParams().WithContext(ctx).WithIds([]string{roleId}),
-	)
+	response, err := getUserRolesByIds(ctx, client, []string{roleId})
 
 	if err != nil {
 		plugin.Logger(ctx).Error("crowdstrike_host.getCrowdStrikeUserRole", "get_role_error", err)
 		return nil, err
 	}
 
-	return response.Payload.Resources[0], nil
+	return response[0], nil
+}
+
+func getUserRolesByIds(ctx context.Context, client *client.CrowdStrikeAPISpecification, ids []string) ([]*models.DomainUserRole, error) {
+	response, err := client.UserManagement.GetRoles(
+		user_management.NewGetRolesParams().WithContext(ctx).WithIds(ids),
+	)
+	if err != nil {
+		plugin.Logger(ctx).Error("crowdstrike_host.getUsersByIds", "RetrieveUserError", err)
+		return nil, err
+	}
+	if err = falcon.AssertNoError(response.Payload.Errors); err != nil {
+		plugin.Logger(ctx).Error("crowdstrike_host.getUsersByIds", "RetrieveUserError", err)
+		return nil, err
+	}
+
+	return response.Payload.Resources, nil
 }
