87f4ce4d72907d83dba1ff17c8c56d98a875ec56
Binaek Sarkar <binaek@turbot.com>
2022-06-30 17:49:36 +0530

    detection table comments

--- crowdstrike/table_crowdstrike_detection.go
+++ crowdstrike/table_crowdstrike_detection.go
@@ -61,22 +61,22 @@ func tableCrowdStrikeDetection(_ context.Context) *plugin.Table {
 		Columns: []*plugin.Column{
 			{Name: "adversary_ids", Description: "If behaviors or indicators in a detection are attributed to an adversary that is tracked by CrowdStrike Falcon Intelligence, those adversaries will have an ID associated with them. These IDs are found in a detection's metadata which can be viewed using the Detection Details API.", Type: proto.ColumnType_JSON, Transform: transform.FromJSONTag()},
 			{Name: "assigned_to_name", Description: "The human-readable name of the user to whom the detection is currently assigned.", Type: proto.ColumnType_STRING},
-			{Name: "assigned_to_uid", Description: "TODO", Type: proto.ColumnType_STRING, Transform: transform.FromJSONTag()},
-			{Name: "behaviors", Description: "TODO", Type: proto.ColumnType_JSON},
-			{Name: "behaviors_processed", Description: "TODO", Type: proto.ColumnType_JSON},
+			{Name: "assigned_to_uid", Description: "The UID of the user to whom the detection is currently assigned.", Type: proto.ColumnType_STRING, Transform: transform.FromJSONTag()},
+			{Name: "behaviors", Description: "Behavorial details of the detection.", Type: proto.ColumnType_JSON},
+			{Name: "behaviors_processed", Description: "The processed behaviors.", Type: proto.ColumnType_JSON},
 			{Name: "cid", Description: "Your organization's customer ID (CID).", Type: proto.ColumnType_STRING},
-			{Name: "created_timestamp", Description: "TODO", Type: proto.ColumnType_TIMESTAMP, Transform: transform.From(func(ctx context.Context, td *transform.TransformData) (interface{}, error) {
+			{Name: "created_timestamp", Description: "Timestamp when this detection was first created.", Type: proto.ColumnType_TIMESTAMP, Transform: transform.From(func(ctx context.Context, td *transform.TransformData) (interface{}, error) {
 				breach := td.HydrateItem.(*models.DomainAPIDetectionDocument)
 				return transformStrFmtDateTime(ctx, *breach.CreatedTimestamp)
 			})},
 			{Name: "detection_id", Description: "The ID of the detection. This ID can be used in conjunction with other APIs, such as the Detection Details API, or the Resolve Detection API.", Type: proto.ColumnType_STRING},
-			{Name: "device", Description: "TODO", Type: proto.ColumnType_JSON},
-			{Name: "email_sent", Description: "TODO", Type: proto.ColumnType_BOOL},
+			{Name: "device", Description: "The device where this was detected.", Type: proto.ColumnType_JSON},
+			{Name: "email_sent", Description: "Whether email was sent when this was detected.", Type: proto.ColumnType_BOOL},
 			{Name: "first_behavior", Description: "When a detection has more than one associated behavior, this field captures the timestamp of the first behavior.", Type: proto.ColumnType_TIMESTAMP, Transform: transform.From(func(ctx context.Context, td *transform.TransformData) (interface{}, error) {
 				breach := td.HydrateItem.(*models.DomainAPIDetectionDocument)
 				return transformStrFmtDateTime(ctx, *breach.FirstBehavior)
 			})},
-			{Name: "host_info", Transform: transform.FromField("Hostinfo"), Description: "TODO", Type: proto.ColumnType_JSON},
+			{Name: "host_info", Transform: transform.FromField("Hostinfo"), Description: "Information about the host where this was detected.", Type: proto.ColumnType_JSON},
 			{Name: "last_behavior", Description: "	When a detection has more than one associated behavior, this field captures the timestamp of the last behavior.", Type: proto.ColumnType_TIMESTAMP, Transform: transform.From(func(ctx context.Context, td *transform.TransformData) (interface{}, error) {
 				breach := td.HydrateItem.(*models.DomainAPIDetectionDocument)
 				return transformStrFmtDateTime(ctx, *breach.LastBehavior)
@@ -84,11 +84,11 @@ func tableCrowdStrikeDetection(_ context.Context) *plugin.Table {
 			{Name: "max_confidence", Description: "When a detection has more than one associated behavior with varying confidence levels, this field captures the highest confidence value of all behaviors. Value can be any integer between 1-100.", Type: proto.ColumnType_INT},
 			{Name: "max_severity", Description: "When a detection has more than one associated behavior with varying severity levels, this field captures the highest severity value of all behaviors. Value can be any integer between 1-100.", Type: proto.ColumnType_INT},
 			{Name: "max_severity_display_name", Transform: transform.FromField("MaxSeverityDisplayname"), Description: "The name used in the UI to determine the severity of the detection. Values include Critical, High, Medium, and Low", Type: proto.ColumnType_STRING},
-			{Name: "overwatch_notes", Description: "TODO", Type: proto.ColumnType_STRING},
-			{Name: "quarantined_files", Description: "TODO", Type: proto.ColumnType_JSON},
+			{Name: "overwatch_notes", Description: "Notes from Falcon Overwatch.", Type: proto.ColumnType_STRING},
+			{Name: "quarantined_files", Description: "Files that have been quarantined.", Type: proto.ColumnType_JSON},
 			{Name: "seconds_to_resolved", Description: "Time that it took to move a detection from newand one of the resolved states (true_positive, false_positive, and ignored).", Type: proto.ColumnType_INT},
 			{Name: "seconds_to_triaged", Description: "Time that it took to move a detection from new to in_progress.", Type: proto.ColumnType_INT},
-			{Name: "show_in_ui", Description: "TODO", Type: proto.ColumnType_BOOL},
+			{Name: "show_in_ui", Description: "Whether this is shown in the User Interface", Type: proto.ColumnType_BOOL},
 			{Name: "status", Description: "The current status of the detection. Values include new, in_progress, true_positive, false_positive, and ignored.", Type: proto.ColumnType_STRING},
 			// Steampipe standard columns
 			{Name: "title", Description: "Title of the resource.", Type: proto.ColumnType_STRING, Transform: transform.FromField("AssignedToName")},
