35c1de7fc131e846be1fb2c91bc70179ed2c5256
Luís Teles <167199412+l-teles@users.noreply.github.com>
2025-09-04 00:16:09 +0100

    Fix `strfmtDatetimeTransformer`

--- crowdstrike/common_transform.go
+++ crowdstrike/common_transform.go
@@ -9,9 +9,45 @@ import (
 )
 
 func strfmtDatetimeTransformer(ctx context.Context, td *transform.TransformData) (interface{}, error) {
-	return td.Value.(*strfmt.DateTime).String(), nil
+	if td == nil || td.Value == nil {
+		return nil, nil
+	}
+
+	switch v := td.Value.(type) {
+	case *strfmt.DateTime:
+		if v == nil {
+			return nil, nil
+		}
+		return v.String(), nil
+	case strfmt.DateTime:
+		return v.String(), nil
+	default:
+		return nil, nil
+	}
 }
 func epochTimestampTransformer(ctx context.Context, td *transform.TransformData) (interface{}, error) {
-	v := td.Value.(*int64)
-	return time.Unix(*v, 0).Format(time.RFC3339), nil
+	if td == nil || td.Value == nil {
+		return nil, nil
+	}
+
+	switch v := td.Value.(type) {
+	case *int64:
+		if v == nil {
+			return nil, nil
+		}
+		return time.Unix(*v, 0).Format(time.RFC3339), nil
+	case int64:
+		return time.Unix(v, 0).Format(time.RFC3339), nil
+	case *int:
+		if v == nil {
+			return nil, nil
+		}
+		return time.Unix(int64(*v), 0).Format(time.RFC3339), nil
+	case int:
+		return time.Unix(int64(v), 0).Format(time.RFC3339), nil
+	case float64:
+		return time.Unix(int64(v), 0).Format(time.RFC3339), nil
+	default:
+		return nil, nil
+	}
 }
